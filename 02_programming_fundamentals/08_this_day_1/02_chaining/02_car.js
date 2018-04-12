07b71b5a7c9bd0e61aceb2cdb9a37e2ec9496fc3
Matthieu Deback <matthieu.deback@decathlon.com>
2018-04-12 17:39:14 +0200

    corrigé

--- 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
+++ 02_programming_fundamentals/08_this_day_1/02_chaining/02_car.js
@@ -1,3 +1,48 @@
 // See Sparta courses for the exercise summary
 
+
+const car = {
+
+  speed: 59,
+  time: 40,
+  count: 56,
+
+  start: function() {
+    this.speed = 0;
+    this.time = 0;
+    this.count = 0;
+    return this;
+  },
+
+  changeSpeed: function (newspeed) {
+    this.speed = newspeed;
+    return this;
+  },
+
+  drive: function (travelTime) {
+    this.time = travelTime;
+    this.count += this.speed*(this.time / 60);
+    return this;
+  },
+
+  showDistance: function () {
+    console.log(this.count + " Km");
+  }
+
+
+};
+
+//
+// car.start();
+// car.changeSpeed(130);
+// car.drive(42);
+// car.showDistance();
+
+car.start().changeSpeed(130).drive(45).showDistance();
+
+console.log(car.showDistance());
+
+
+
+
 module.exports = car;
