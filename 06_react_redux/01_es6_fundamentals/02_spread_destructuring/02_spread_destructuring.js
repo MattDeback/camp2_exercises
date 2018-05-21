c410645f9895e0e8ccf1424618e23a57808fbc44
Matthieu Deback <matthieu.deback@decathlon.com>
2018-05-21 18:28:05 +0200

    exoscorrigéfinal

--- 06_react_redux/01_es6_fundamentals/02_spread_destructuring/02_spread_destructuring.js
+++ 06_react_redux/01_es6_fundamentals/02_spread_destructuring/02_spread_destructuring.js
@@ -1,25 +1,27 @@
-function go(options) {
-  let {
-    speed = 4,
-    enable: { hyperdrive = false, frobnifier = true}
-  } = options;
+// function go(options) {
+//   let {
+//     speed = 4,
+//     enable: { hyperdrive = false, frobnifier = true}
+//   } = options;
+//
+//   console.log("speed=", speed, "hyperdrive:", hyperdrive, "frobnifier:", frobnifier);
+// }
+//
+// go({ speed: 5 });
 
-  console.log("speed=", speed, "hyperdrive:", hyperdrive, "frobnifier:", frobnifier);
-}
+// function lastIndexOf(arr, elt, start=arr.length-1) {
+//   for (let i = start - 1; i >= 0; i--) {
+//     if (arr[i] === elt) {
+//       return i;
+//     }
+//   }
+//
+//   return -1;
+// }
+//
+// .lenght-1
 
-go({ speed: 5 });
-
-function lastIndexOf(arr, elt, start) {
-  for (let i = start - 1; i >= 0; i--) {
-    if (arr[i] === elt) {
-      return i;
-    }
-  }
-
-  return -1;
-}
-
-console.log(lastIndexOf([1, 2, 1, 2], 2));
+// console.log(lastIndexOf([1, 2, 1, 2], 2));
 
 function replace(array, from, to, elements) {
   array.splice.apply(array, [from, to - from].concat(elements));
